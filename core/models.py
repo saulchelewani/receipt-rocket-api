194bbc7724b977b6506176de6db7b6e9199f92b4
Saul Chelewani <saulchelewani@gmail.com>
2025-05-21 02:08:01 +0200

    Add tenant management with API routes and tests
    Introduce functionality for tenant management, including API endpoints for creating and listing tenants. Add Pydantic schemas, database setup, authentication, and unit tests to ensure proper operation. Initial project structure and configurations are included.

---
+++ core/models.py
@@ -0,0 +1,65 @@
+import uuid
+
+from sqlalchemy import Column, String, ForeignKey, Boolean, UUID, Table
+from sqlalchemy.orm import Mapped, relationship
+
+from core.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True, index=True, unique=True,
+                              nullable=False)
+    email: Mapped[str] = Column(String, unique=True, index=True)
+    name: Mapped[str] = Column(String, nullable=True)
+    hashed_password: Mapped[str] = Column(String)
+    tenant_id: Mapped[UUID] = Column(UUID(as_uuid=True), ForeignKey("tenants.id"), nullable=True)
+    role_id: Mapped[UUID] = Column(UUID(as_uuid=True), ForeignKey("roles.id"))
+    scope: Mapped[str] = Column(String, nullable=True)
+
+    role: Mapped["Role"] = relationship("Role", back_populates="users")
+    tenant: Mapped["Tenant"] = relationship("Tenant", back_populates="users")
+
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id: Mapped[UUID] = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True, index=True, unique=True,
+                              nullable=False)
+    name: Mapped[str] = Column(String, unique=True, index=True, nullable=False)
+    description: Mapped[str] = Column(String, nullable=True)
+
+    users: Mapped[list["User"]] = relationship("User", back_populates="role")
+    routes: Mapped[list["Route"]] = relationship("Route", secondary="role_route_association", back_populates="roles")
+
+
+class Tenant(Base):
+    __tablename__ = "tenants"
+
+    id: Mapped[UUID] = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True, index=True, unique=True,
+                              nullable=False)
+    name: Mapped[str] = Column(String, unique=True, index=True, nullable=False)
+    code: Mapped[str] = Column(String, unique=True, index=True, nullable=False)
+
+    users: Mapped[list["User"]] = relationship("User", back_populates="tenant")
+
+
+class Route(Base):
+    __tablename__ = "routes"
+
+    id: Mapped[UUID] = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True, index=True, unique=True,
+                              nullable=False)
+    path: Mapped[str] = Column(String, unique=False, index=True)  # The route path (e.g., "/tasks/")
+    method: Mapped[str] = Column(String)  # The HTTP method (e.g., "GET", "POST")
+    action: Mapped[str] = Column(String, nullable=False)  # (e.g. delete task)
+    name: Mapped[str] = Column(String, nullable=True)
+
+    roles: Mapped[list["Role"]] = relationship("Role", secondary="role_route_association", back_populates="routes")
+
+role_route_association = Table(
+    "role_route_association",
+    Base.metadata,
+    Column("role_id", UUID(as_uuid=True), ForeignKey("roles.id"), primary_key=True),
+    Column("route_id", UUID(as_uuid=True), ForeignKey("routes.id"), primary_key=True),
+)
