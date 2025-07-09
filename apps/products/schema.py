ba6dc540c46f6361859c62efd0f1cf985ed409a7
Saul Chelewani <saulchelewani@gmail.com>
2025-07-09 15:03:44 +0200

    fix: site ID to be parsed as a string

--- apps/products/schema.py
+++ apps/products/schema.py
@@ -10,7 +10,7 @@ class ProductBase(BaseModel):
     unit_price: float | None = None
     quantity: int | None = None
     unit_of_measure: str | None = None
-    site_id: UUID | None = None
+    site_id: str | None = None
     expiry_date: datetime | None = None
     minimum_stock_level: int | None = None
     tax_rate_id: str | None = None
